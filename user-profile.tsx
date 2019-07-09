c6993553e4fefae6b9aa8cb8bf516d78d786e3ea
Tung <khuatthanhtung09@gmail.com>
2019-07-09 09:59:07 +0300

    Added user profile view

---
+++ user-profile.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity, Image } from 'react-native';
+
+export default function UserProfile() {
+  return (
+    <View>
+      <Text>Hello world!</Text>
+    </View>
+  );
+}
