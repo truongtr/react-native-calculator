6d3b6e8f5347ff76f73b92f9a7976bebe9900041
Tung <khuatthanhtung09@gmail.com>
2019-07-09 09:58:06 +0300

    Added navigation

--- App.tsx
+++ App.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import ListItem from './list-item';
+import UserProfile from './user-profile';
 
-export default function App() {
+import {createStackNavigator, createAppContainer} from 'react-navigation';
+
+const AppNavigator = createStackNavigator({
+  Home: {screen: App},
+  User: {screen: UserProfile}
+});
+
+export default createAppContainer(AppNavigator);
+
+export default function App(props) {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <ListItem navigation={props.navigation}/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
